a77a5f6afc3a47e14a7b697be3b792a357616d82
wulang <1359208000@qq.com>
2018-07-09 10:02:11 +0800

    error

--- dubbo-provider/src/main/java/com/wulang/MyDubboProvider.java
+++ dubbo-provider/src/main/java/com/wulang/MyDubboProvider.java
@@ -21,8 +21,8 @@ public class MyDubboProvider {
 //        registry.setPassword("bbb");
 
 // 服务提供者协议配置
-        ProtocolConfig protocol = new ProtocolConfig();
-        protocol.setName("zookeeper");
+//        ProtocolConfig protocol = new ProtocolConfig();
+//        protocol.setName("zookeeper");
 
 // 注意：ServiceConfig为重对象，内部封装了与注册中心的连接，以及开启服务端口
 
@@ -30,10 +30,11 @@ public class MyDubboProvider {
         ServiceConfig<Hello> service = new ServiceConfig<>(); // 此实例很重，封装了与注册中心的连接，请自行缓存，否则可能造成内存和连接泄漏
         service.setApplication(application);
         service.setRegistry(registry); // 多个注册中心可以用setRegistries()
-        service.setProtocol(protocol); // 多个协议可以用setProtocols()
+//        service.setProtocol(protocol); // 多个协议可以用setProtocols()
         service.setInterface(Hello.class);
-        service.setRef(xxxService);
-        service.setVersion("1.0.0");
+        Hello hi=new HelloImpl();
+        service.setRef(hi);
+//        service.setVersion("1.0.0");
 
 // 暴露及注册服务
         service.export();
