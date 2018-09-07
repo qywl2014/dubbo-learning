4c5b72a773b2984b9a76f867a6523c9d945e3aea
wulang <1359208000@qq.com>
2018-09-07 17:23:38 +0800

    temp

--- dubbo-provider/src/main/java/com/wulang/Application.java
+++ dubbo-provider/src/main/java/com/wulang/Application.java
@@ -3,7 +3,7 @@ package com.wulang;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class Test {
+public class Application {
     public static void main(String[] arg){
 
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext(new String[] {"classpath*:applicationContext.xml" });
